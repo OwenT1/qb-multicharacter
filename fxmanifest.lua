0634e966ececd973feba0f70af6a9954b5273a30
OwenT1 <106706707+OwenT1@users.noreply.github.com>
2022-06-02 22:56:29 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,9 +6,7 @@ version '1.1.0'
 
 shared_script 'config.lua'
 client_script 'client/main.lua'
-server_scripts  {
-    '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+server_scripts 'server/main.lua'
 }
 
 ui_page 'html/index.html'
